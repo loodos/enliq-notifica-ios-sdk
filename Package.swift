dc0cc0ffde9c5f194824e7588f90ff613f46db65
SercanKaya0 <sercan.kaya@icloud.com>
2025-10-22 16:27:31 +0300

    Release 2.0.6

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "EIQNotifica",
-            url: "https://github.com/loodos/enliq-notifica-ios-sdk/releases/download/v2.0.0/eiqnotifica.xcframework.zip",
-            checksum: "b3b9a2004d61cf6fb61d6ebb59689ec3170a8161ba2954ae0cfe4f603dd0f212"
+            url: "https://github.com/loodos/enliq-notifica-ios-sdk/releases/download/2.0.6/eiqnotifica.xcframework.zip",
+            checksum: "cbf6227aaea82933c787b5cd2f8d7714e4273fdfeb4ecf9b07bc42a1e551e806"
         )
     ]
 )
